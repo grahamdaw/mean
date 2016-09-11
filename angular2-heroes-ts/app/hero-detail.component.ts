c18dd80bd4574bd89a1c7c199b56a725b1d0a497
Graham Daw <grahamdaw@users.noreply.github.com>
2016-09-11 09:56:08 -0700

    Updates for component split & routing

--- angular2-heroes-ts/app/hero-detail.component.ts
+++ angular2-heroes-ts/app/hero-detail.component.ts
@@ -1,21 +1,34 @@
-import { Component, Input } from '@angular/core';
+// Keep the Input import for now, we'll remove it later:
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-hero-detail',
-  template: `
-    <div *ngIf="hero">
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name"/>
-      </div>
-    </div>
-  `
+  templateUrl: 'app/hero-detail.component.html',
 })
-export class HeroDetailComponent {
-  @Input()
-  hero: Hero;
+
+export class HeroDetailComponent implements OnInit {
+
+  hero: Hero = null;
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.route.params.forEach((params: Params) => {
+      let id = +params['id']; //+ converts to int
+      this.heroService.getHero(id).then(hero => this.hero = hero);
+    });
+  }
+
+  goBack(): void {
+    // Can take us out of app, better methods available
+    window.history.back();
+  }
+
 }
